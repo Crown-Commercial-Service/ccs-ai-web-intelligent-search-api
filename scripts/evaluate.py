ee95cabcf76ff355b4fa59b16b695f77c122ab2e
SamuelHLewis <sam.lewis1@crowncommercial.gov.uk>
2026-02-27 16:13:27 +0000

    Add RM number label and logic

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -81,10 +81,14 @@ for i in truthset["Question"]:
     # build the graph each time, to clear context
     memory = MemorySaver() # for in-memory state handling
     graph = build_graph(llm=llm, vector_store=vector_store, checkpointer=memory)
-    response = answer_once(graph, i)
+    rm_label_result = run_rm_labeller(pydantic_rm_labeller_model, rm_descriptions, i)
+    config = {"rm_filter": rm_label_result.rm_number}
+    response = answer_once(graph, i, config)
     responses.append(response)
     if len(responses) % 10 == 0:
         print(f"Responses generated for {len(responses)} questions")
+truthset["RM Number Result"] = rm_label_result.rm_number
+truthset["RM Number Reasoning"] = rm_label_result.reasoning
 truthset["Answer"] = [i["answer"] for i in responses]
 truthset["Retrieved Files"] = [i["source_names"] for i in responses]
 truthset["Retrieved Contents"] = [i["source_contents"] for i in responses]
